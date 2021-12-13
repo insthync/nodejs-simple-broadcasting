6a35102774f15861bcdb5e725a657fdf94281116
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2021-12-13 15:58:30 +0700

    Done :P

---
+++ index.js
@@ -0,0 +1,20 @@
+const { Server } = require("socket.io");
+require('dotenv').config();
+
+const port = Number(process.env.SERVER_PORT || 8212);
+
+const io = new Server({ /* options */ });
+
+io.on("connection", (socket) => {
+    socket.on("all", (data) => {
+        // Broadcast to all clients
+        io.emit("msg", data);
+      socket.username = username;
+    });
+    socket.on("other", (data) => {
+        // Broadcast to other clients
+        socket.broadcast.emit("msg", data);
+    });
+});
+
+io.listen(port);
