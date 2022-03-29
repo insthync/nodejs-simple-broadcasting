f4dc0d3285f39fafe78fde9c982695277bd19786
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2022-03-29 16:32:00 +0700

    Implements secure server

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const { Server } = require("socket.io");
+const fs = require("fs");
+const https = require("https");
+const http = require("http");
 require('dotenv').config();
 
-const port = Number(process.env.SERVER_PORT || 8212);
-
-const io = new Server({ /* options */ });
+const io = new Server();
 
 io.on("connection", (socket) => {
     socket.on("all", (data) => {
@@ -16,5 +17,25 @@ io.on("connection", (socket) => {
     });
 });
 
-io.listen(port);
-console.log(`Simple Socket.io Broadcasting is listening on port ${port}`)
+const port = Number(process.env.SERVER_PORT || 8000)
+const useHttps = Number(process.env.USE_HTTPS || 0) > 0
+const keyFilePath = process.env.HTTPS_KEY_FILE_PATH || ''
+const certFilePath = process.env.HTTPS_CERT_FILE_PATH || ''
+const httpsPort = Number(process.env.HTTPS_SERVER_PORT || 8080)
+
+const httpServer = http.createServer()
+io.attach(httpServer)
+httpServer.listen(port, () => {
+    console.log(`Simple Socket.io Broadcasting is listening on port ${port}`);
+})
+
+if (useHttps) {
+    const httpsServer = https.createServer({
+        key: fs.readFileSync(keyFilePath),
+        cert: fs.readFileSync(certFilePath),
+    })
+    io.attach(httpsServer)
+    httpsServer.listen(httpsPort, () => {
+        console.log(`Simple Socket.io Broadcasting is listening on port ${httpsPort}`);
+    })
+}
