39e737da9c72f3538a18138c9ed27ee20d68eec3
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2022-09-28 07:04:06 +0700

    Change to use typescript

--- index.ts
+++ index.ts
@@ -1,21 +1,22 @@
-const { Server } = require("socket.io");
-const fs = require("fs");
-const https = require("https");
-const http = require("http");
-require('dotenv').config();
+import { Server } from "socket.io"
+import * as dotenv from 'dotenv'
+import fs from "fs"
+import https from "https"
+import http from "http"
 
-const io = new Server();
+dotenv.config()
+const io = new Server()
 
 io.on("connection", (socket) => {
     socket.on("all", (data) => {
         // Broadcast to all clients
-        io.emit("msg", data);
-    });
+        io.emit("msg", data)
+    })
     socket.on("other", (data) => {
         // Broadcast to other clients
-        socket.broadcast.emit("msg", data);
-    });
-});
+        socket.broadcast.emit("msg", data)
+    })
+})
 
 const port = Number(process.env.SERVER_PORT || 8000)
 const useHttps = Number(process.env.USE_HTTPS || 0) > 0
@@ -26,7 +27,7 @@ const httpsPort = Number(process.env.HTTPS_SERVER_PORT || 8080)
 const httpServer = http.createServer()
 io.attach(httpServer)
 httpServer.listen(port, () => {
-    console.log(`Simple Socket.io Broadcasting is listening on port ${port}`);
+    console.log(`Simple Socket.io Broadcasting is listening on port ${port}`)
 })
 
 if (useHttps) {
@@ -36,6 +37,6 @@ if (useHttps) {
     })
     io.attach(httpsServer)
     httpsServer.listen(httpsPort, () => {
-        console.log(`Simple Socket.io Broadcasting is listening on port ${httpsPort}`);
+        console.log(`Simple Socket.io Broadcasting is listening on port ${httpsPort}`)
     })
 }
